edce6308523b7a1a5ebb5a6d1a31742eaf66837a
CPGX <67716127+CPGX@users.noreply.github.com>
2022-06-01 21:22:17 -0400

    Create IntroductionTakeFour.java

---
+++ IntroductionTakeFour.java
@@ -0,0 +1,27 @@
+public class IntroductionTakeFour {
+
+    public static void myName(String firstName, String middleName, String lastName){
+
+       String fullName = firstName + " " + middleName + " " + lastName;
+    
+       System.out.println(fullName);
+    
+    }
+
+    public static void hobbies(String favSport, String favSportTwo, String insutrment) {
+
+     // hobbies myHobbies = new hobbies();
+
+
+    }
+
+
+public static void main(String[] args) {
+
+    System.out.println("My name is ");
+    myName("Courtney", "Ping", "Giroux");
+
+}
+
+        
+}
