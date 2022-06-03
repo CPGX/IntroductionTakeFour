8472e79a4750dd11d6ac1aee17e21033350496cd
CPGX <67716127+CPGX@users.noreply.github.com>
2022-06-03 14:18:57 -0400

    Update

--- IntroductionTakeFour.java
+++ IntroductionTakeFour.java
@@ -17,6 +17,43 @@ public class IntroductionTakeFour {
 
     }
 
+    public static void favFoods(String food, String drink, String snack) {
+
+        String myFavFoods = food + " " + drink + " " + snack;
+        
+        System.out.println(myFavFoods);
+    }
+
+    public static void todaysTea (String myTeaToday) {
+
+        myTeaToday = "Oolong";
+
+        switch (myTeaToday) {
+
+            case "Decaf Green":
+            System.out.println("Hopefully I won't get drunk");
+            break;
+        
+            case "Jasmine Green":
+            System.out.println("My favorite");
+            break;
+
+            case "Oolong":
+            System.out.println("I love how rich and earthy oolong tea is");
+            break;
+
+            case "Peppermint":
+            System.out.println("You must be awufully tired");
+            break;
+            
+            default:
+            System.out.println("You have no tea. Disgraceful. Go make yourself a cup");
+
+
+        }
+
+    }
+
 
 public static void main(String[] args) {
 
@@ -31,7 +68,12 @@ public static void main(String[] args) {
     hobbies("Golf", "Skiing", "Flute");
     System.out.println("And, well, also procrastinating.");
 
-    
+    System.out.println("Some of my favoirte things to eat are");
+    favFoods("Ramen", "Tea", "Goldfish");
+    System.out.println("We can't forget, goldfish are the revolution.");
+
+    System.out.println("My tea for today is ");
+    todaysTea("Oolong");
 
 }
 
