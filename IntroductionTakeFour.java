d8401071128c7abc920b708c40be8929259a1ddd
CPGX <67716127+CPGX@users.noreply.github.com>
2022-06-03 14:28:41 -0400

    Add More Tea

--- IntroductionTakeFour.java
+++ IntroductionTakeFour.java
@@ -1,5 +1,8 @@
 public class IntroductionTakeFour {
 
+    public static String myTeaToday = "Oolong";
+
+
     public static void myName(String firstName, String middleName, String lastName){
 
        String fullName = firstName + " " + middleName + " " + lastName;
@@ -26,26 +29,57 @@ public class IntroductionTakeFour {
 
     public static void todaysTea (String myTeaToday) {
 
-        myTeaToday = "Oolong";
-
+    
         switch (myTeaToday) {
 
             case "Decaf Green":
-            System.out.println("Hopefully I won't get drunk");
+            System.out.println("Hopefully I won't get drunk.");
             break;
         
             case "Jasmine Green":
-            System.out.println("My favorite");
+            System.out.println("My favorite.");
             break;
 
             case "Oolong":
-            System.out.println("I love how rich and earthy oolong tea is");
+            System.out.println("I love how rich and earthy oolong tea is.");
             break;
 
             case "Peppermint":
-            System.out.println("You must be awufully tired");
+            System.out.println("I must be awufully tired.");
+            break;
+
+            case "Peachy Green": 
+            System.out.println("Richer than ginger peach, but more refreshing than most.");
+            break;
+
+            case "Jasmine White":
+            System.out.println("So light and refreshing!");
+            break;
+
+            case "Chamomile":
+            System.out.println("It's quite relaxing, so perfect before bed.");
+            break;
+
+            case "Matcha":
+            System.out.println("Are you as envious as your tea?");
+            break;
+
+            case "Thai Iced tea":
+            System.out.println("A real treat. Don't drink too much though!");
+            break;
+
+            case "Bubble tea":
+            System.out.println("So good. Bamee's bubble tea is top notch");
             break;
             
+            case "Earl Grey":
+            System.out.println("Dark and moody, but somehow refrehsing. Pefect for the morning");
+            break;
+
+            case "Lady Grey":
+            System.out.println("Slightly more refreshign than Earl Grey, also good for the morning.");
+            break;
+
             default:
             System.out.println("You have no tea. Disgraceful. Go make yourself a cup");
 
@@ -72,7 +106,7 @@ public static void main(String[] args) {
     favFoods("Ramen", "Tea", "Goldfish");
     System.out.println("We can't forget, goldfish are the revolution.");
 
-    System.out.println("My tea for today is ");
+    System.out.println("My tea for today is " + myTeaToday);
     todaysTea("Oolong");
 
 }
